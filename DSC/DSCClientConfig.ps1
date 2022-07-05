f39556780d542f5bd6e8ae8d4f203432eff9b76c
evanjd711 <evanjd711@gmail.com>
2022-07-04 23:56:40 -0700

    Initial DSC Configs

---
+++ DSC/DSCClientConfig.ps1
@@ -0,0 +1,33 @@
+﻿[DSCLocalConfigurationManager()]
+Configuration PullClientConfigID
+{
+    param(
+        [string[]]$NodeName = 'localhost',
+
+        [Parameter(Mandatory)]
+        [ValidateNotNullOrEmpty()]
+        [string] $DSCServerFQDN,
+        [Parameter(Mandatory)]
+        [ValidateNotNullOrEmpty()]
+        [string[]] $Configurations
+    )
+
+    Node localhost
+    {
+        Settings
+        {
+            RefreshMode = 'Pull'
+            RefreshFrequencyMins = 30
+            RebootNodeIfNeeded = $true
+            ConfigurationMode = "ApplyAndAutoCorrect"
+        }
+
+        ConfigurationRepositoryWeb PullSrv
+        {
+            ServerURL = "http://$DSCServerFQDN`:8080/PSDSCPullServer.svc"
+            RegistrationKey = '05916093-ed87-46be-9c72-1a862d3c90d1'
+            ConfigurationNames = $Configurations
+            AllowUnsecureConnection = $true
+        }
+    }
+}
---
+++ DSC/DscWebServiceRegistration.ps1
@@ -0,0 +1,47 @@
+﻿configuration New_xDscPullServer
+{
+    param
+    (
+        [string[]]$NodeName = 'localhost',
+
+        [Parameter(Mandatory)]
+        [ValidateNotNullOrEmpty()]
+        [string] $RegistrationKey
+    )
+
+    Import-DscResource -ModuleName xPSDesiredStateConfiguration
+    Import-DscResource -ModuleName PSDesiredStateConfiguration
+    Import-DscResource -ModuleName NetworkingDsc
+
+    Node $NodeName
+    {
+        WindowsFeature DSCServiceFeature
+        {
+            Ensure = 'Present'
+            Name = 'DSC-Service'
+        }
+
+        xDscWebService PSDSCPullServer
+        {
+            Ensure = 'Present'
+            EndpointName = 'PSDSCPullServer'
+            Port = 8080
+            PhysicalPath = "$env:SystemDrive\inetpub\PSDSCPullServer"
+            CertificateThumbPrint = "BEF14AEA377DDF6C00271A435BF2061021FFCB1B"
+            ModulePath = "$env:ProgramFiles\WindowsPowerShell\DscService\Modules"
+            ConfigurationPath = "$env:ProgramFiles\WindowsPowerShell\DscService\Configuration"
+            State = 'Started'
+            DependsOn = '[WindowsFeature]DSCServiceFeature'
+            UseSecurityBestPractices = $false
+            ConfigureFirewall = $false
+        }
+
+        File RegistrationKeyFile
+        {
+            Ensure = 'Present'
+            Type = 'File'
+            DestinationPath = "$env:ProgramFiles\WindowsPowerShell\DscService\RegistrationKeys.txt"
+            Contents = $RegistrationKey
+        }
+    }
+}
