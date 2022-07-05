99ac429ddeb0bf572f4e14fe6ac1d50b40cc5250
evanjd711 <evanjd711@gmail.com>
2022-07-05 01:24:50 -0700

    Improve ClientConfig

--- DSC/DSCClientConfig.ps1
+++ DSC/DSCClientConfig.ps1
@@ -9,7 +9,10 @@ Configuration PullClientConfigID
         [string] $DSCServerFQDN,
         [Parameter(Mandatory)]
         [ValidateNotNullOrEmpty()]
-        [string[]] $Configurations
+        [string[]] $Configurations,
+        [Parameter(Mandatory)]
+        [ValidateNotNullOrEmpty()]
+        [string[]] $RegistrationKey
     )
 
     Node localhost
@@ -25,7 +28,7 @@ Configuration PullClientConfigID
         ConfigurationRepositoryWeb PullSrv
         {
             ServerURL = "http://$DSCServerFQDN`:8080/PSDSCPullServer.svc"
-            RegistrationKey = '05916093-ed87-46be-9c72-1a862d3c90d1'
+            RegistrationKey = $RegistrationKey
             ConfigurationNames = $Configurations
             AllowUnsecureConnection = $true
         }
