832116f29b3ecf99e64c46e498514a03ad3ac38a
evanjd711 <evanjd711@gmail.com>
2022-07-05 00:18:23 -0700

    Update WebServer

--- DSC/Configuration/WebServer/WebServer.ps1
+++ DSC/Configuration/WebServer/WebServer.ps1
@@ -1,19 +1,26 @@
 Configuration WebServer
 {
-    param ($MachineName)
+    param ($MachineName='localhost')
 
     Node $MachineName
     {
         WindowsFeature IIS
         {
-            Ensure = "Present"
-            Name = "Web-Server"
+            Ensure  = "Present"
+            Name    = "Web-Server"
         }
 
         WindowsFeature ASP
         {
-            Ensure = "Present"
-            Name = "web-Asp-Net45"
+            Ensure  = "Present"
+            Name    = "web-Asp-Net45"
+        }
+
+        WindowsFeature WebManagement
+        {
+            Ensure  = "Present"
+            Name    = "Web-Mgmt-Tools"
+            IncludeAllSubFeature = $true
         }
     }
 }
