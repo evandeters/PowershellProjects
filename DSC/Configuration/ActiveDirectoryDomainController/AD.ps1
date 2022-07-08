6e3b15caa6ac00961673bf677304fae86e1b2cd8
Evan Deters <evanjd711@gmail.com>
2022-07-07 21:58:25 -0700

    AD script for home

---
+++ DSC/Configuration/ActiveDirectoryDomainController/AD.ps1
@@ -0,0 +1,75 @@
+#Import configuration database from CSV file  
+$Data =   
+$ComputerName = $Data.ComputerName  
+$Password = $Data.NewDSRMAdminPassword  
+$DomainName = $Data.DomainName  
+$MOFfiles = $Data.MOFFileLocation  
+#Encrypt Passwords  
+$Cred = ConvertTo-SecureString -String $Password -Force -AsPlainText  
+$DomainCredential = New-Object System.Management.Automation.PSCredential ("$(($DomainName -split '\.')[0])\Administrator", $Cred)  
+$DSRMpassword = New-Object System.Management.Automation.PSCredential ('No UserName', $Cred)  
+#Create connection to remote computer  
+$RemoteAdministratorCred = Get-Credential -UserName Administrator -Message "$ComputerName Administrator password"  
+$CimSession = New-CimSession -ComputerName $ComputerName -Credential $RemoteAdministratorCred -Name $ComputerName  
+
+Configuration NewActiveDirectoryConfig {  
+  param (  
+    [Parameter(Mandatory)]   
+    [PSCredential]$DomainCredential,  
+    [Parameter(Mandatory)]   
+    [PSCredential]$DSRMpassword  
+  )  
+  Import-DscResource –ModuleName xActiveDirectory  
+  Node $ComputerName {  
+     #Install Active Directory role and required tools  
+    WindowsFeature ActiveDirectory {  
+      Ensure = 'Present'  
+      Name = 'AD-Domain-Services'  
+    }  
+    WindowsFeature ActiveDirectoryTools {  
+      Ensure = 'Present'  
+      Name = 'RSAT-AD-Tools'  
+      DependsOn = "[WindowsFeature]ActiveDirectory"  
+    }  
+    WindowsFeature DNSServerTools {  
+      Ensure = 'Present'  
+      Name = 'RSAT-DNS-Server'  
+      DependsOn = "[WindowsFeature]ActiveDirectoryTools"  
+    }  
+    WindowsFeature ActiveDirectoryPowershell {  
+      Ensure = "Present"  
+      Name  = "RSAT-AD-PowerShell"  
+      DependsOn = "[WindowsFeature]DNSServerTools"  
+    }  
+     #Configure Active Directory Role   
+    xADDomain RootDomain {  
+      Domainname = $DomainName  
+      SafemodeAdministratorPassword = $DSRMpassword  
+      DomainAdministratorCredential = $DomainCredential  
+      #DomainNetbiosName = ($DomainName -split '\.')[0]  
+      DependsOn = "[WindowsFeature]ActiveDirectory", "[WindowsFeature]ActiveDirectoryPowershell"  
+    }  
+     #LCM Configuration  
+    LocalConfigurationManager {        
+      ActionAfterReboot = 'ContinueConfiguration'        
+      ConfigurationMode = 'ApplyOnly'        
+      RebootNodeIfNeeded = $true        
+    }        
+  }  
+}  
+#Allow plain text password to be stored  
+$ConfigurationData = @{  
+  AllNodes = @(  
+    @{  
+      NodeName = $ComputerName  
+      PSDscAllowPlainTextPassword = $true  
+      DomainName = $DomainName  
+     }  
+  )  
+}  
+#Generate mof files  
+NewActiveDirectoryConfig -DSRMpassword $DSRMpassword -DomainCredential $DomainCredential -OutputPath $MOFfiles -ConfigurationData $ConfigurationData  
+#Configure LCM on remote computer  
+Set-DSCLocalConfigurationManager -Path $MOFfiles –Verbose -CimSession $CimSession  
+#Start Deployment remotely  
+Start-DscConfiguration -Path $MOFfiles -Verbose -CimSession $CimSession -Wait -Force 
