4fe8f13377059504aaba9c85d8b62fd0f081a46f
evanjd711 <evanjd711@gmail.com>
2022-07-09 01:20:47 -0700

    Client

---
+++ RvB/Configurations/PullClientConfigID.ps1
@@ -0,0 +1,50 @@
+﻿[DSCLocalConfigurationManager()]
+Configuration PullClientConfigID
+{
+    param(
+        [string[]]$NodeName = 'localhost',
+
+        [Parameter(Mandatory)]
+        [ValidateNotNullOrEmpty()]
+        [string] $DSCServerFQDN,
+        [Parameter(Mandatory)]
+        [ValidateNotNullOrEmpty()]
+        [string[]] $Configurations,
+        [Parameter(Mandatory)]
+        [ValidateNotNullOrEmpty()]
+        [string] $RegistrationKey
+    )
+    
+    Node $NodeName
+    {
+        Settings
+        {
+            RefreshMode                     = 'Pull'
+            RefreshFrequencyMins            = 30
+            RebootNodeIfNeeded              = $true
+            ConfigurationMode               = "ApplyAndAutoCorrect"
+            AllowModuleOverwrite            = $true
+        }
+
+        ConfigurationRepositoryWeb PullSrv
+        {
+            ServerURL               = "http://$DSCServerFQDN`:8080/PSDSCPullServer.svc"
+            RegistrationKey         = $RegistrationKey
+            ConfigurationNames      = $Configurations
+            AllowUnsecureConnection = $true
+        }
+
+        ResourceRepositoryWeb ResourceSrv
+        {
+            ServerURL               = "http://$DSCServerFQDN`:8080/PSDSCPullServer.svc"
+            RegistrationKey         = $RegistrationKey
+            AllowUnsecureConnection = $true
+        } 
+
+        PartialConfiguration PowerShellWebAccess
+        {
+            Description = "PSWA"
+            ConfigurationSource = @("[ConfigurationRepositoryWeb]PullSrv")
+        }
+    }
+}
