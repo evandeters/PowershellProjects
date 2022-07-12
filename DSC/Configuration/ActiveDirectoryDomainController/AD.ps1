2f2845aa8b9e4ef2935313a67f77609d6bff667c
evanjd711 <evanjd711@gmail.com>
2022-07-12 13:54:31 -0700

    New AD

--- DSC/Configuration/ActiveDirectoryDomainController/AD.ps1
+++ DSC/Configuration/ActiveDirectoryDomainController/AD.ps1
@@ -1,61 +1,51 @@
-Configuration NewActiveDirectoryConfig {  
-  param (  
-    [Parameter(Mandatory)]   
-    [PSCredential]$DomainCredential = "swift",  
-    [Parameter(Mandatory)]   
-    [PSCredential]$DSRMpassword = "swift"
-  )  
-  Import-DscResource -ModuleName xActiveDirectory  
-  Node $ComputerName {  
-    #Install Active Directory role and required tools  
-    WindowsFeature ActiveDirectory {  
-      Ensure = 'Present'  
-      Name   = 'AD-Domain-Services'  
-    }  
-    WindowsFeature ActiveDirectoryTools {  
-      Ensure    = 'Present'  
-      Name      = 'RSAT-AD-Tools'  
-      DependsOn = "[WindowsFeature]ActiveDirectory"  
-    }  
-    WindowsFeature DNSServerTools {  
-      Ensure    = 'Present'  
-      Name      = 'RSAT-DNS-Server'  
-      DependsOn = "[WindowsFeature]ActiveDirectoryTools"  
-    }  
-    WindowsFeature ActiveDirectoryPowershell {  
-      Ensure    = "Present"  
-      Name      = "RSAT-AD-PowerShell"  
-      DependsOn = "[WindowsFeature]DNSServerTools"  
-    }  
-    #Configure Active Directory Role   
-    xADDomain RootDomain {  
-      Domainname                    = 'evan.test'
-      SafemodeAdministratorPassword = $DSRMpassword  
-      DomainAdministratorCredential = $DomainCredential  
-      #DomainNetbiosName = ($DomainName -split '\.')[0]  
-      DependsOn                     = "[WindowsFeature]ActiveDirectory", "[WindowsFeature]ActiveDirectoryPowershell"  
-    }  
-    #LCM Configuration  
-    LocalConfigurationManager {        
-      ActionAfterReboot  = 'ContinueConfiguration'        
-      ConfigurationMode  = 'ApplyOnly'        
-      RebootNodeIfNeeded = $true        
-    }        
-  }  
-}  
-#Allow plain text password to be stored  
-$ConfigurationData = @{  
-  AllNodes = @(  
-    @{  
-      NodeName                    = DC  
-      PSDscAllowPlainTextPassword = $true  
-      DomainName                  = 'evan.test'
-    }  
-  )  
-}  
-#Generate mof files  
-NewActiveDirectoryConfig -DSRMpassword $DSRMpassword -DomainCredential $DomainCredential -OutputPath $MOFfiles -ConfigurationData $ConfigurationData  
-#Configure LCM on remote computer  
-Set-DSCLocalConfigurationManager -Path $MOFfiles –Verbose -CimSession $CimSession  
-#Start Deployment remotely  
-Start-DscConfiguration -Path $MOFfiles -Verbose -CimSession $CimSession -Wait -Force 
+Install-Module -Name ActiveDirectoryDsc -Confirm:$false
+Install-Module -Name xPSDesiredStateConfiguration -Confirm:$false
+
+Configuration ADDomain_NewForest_Config
+{
+    param
+    (
+        [Parameter(Mandatory = $true)]
+        [ValidateNotNullOrEmpty()]
+        [System.Management.Automation.PSCredential]
+        $Credential,
+
+        [Parameter(Mandatory = $true)]
+        [ValidateNotNullOrEmpty()]
+        [System.Management.Automation.PSCredential]
+        $SafeModePassword,
+
+        [Parameter(Mandatory = $true)]
+        [ValidateNotNullOrEmpty()]
+        [String]
+        $DomainName
+    )
+
+    Import-DscResource -ModuleName PSDesiredStateConfiguration
+    Import-DscResource -ModuleName ActiveDirectoryDsc
+
+    node 'localhost'
+    {
+        WindowsFeature 'ADDS'
+        {
+            Name   = 'AD-Domain-Services'
+            Ensure = 'Present'
+        }
+
+        WindowsFeature 'RSAT'
+        {
+            Name   = 'RSAT-AD-PowerShell'
+            Ensure = 'Present'
+        }
+
+        WindowsFeature ''
+
+        ADDomain 'contoso.com'
+        {
+            DomainName                    = $DomainName
+            Credential                    = $Credential
+            SafemodeAdministratorPassword = $SafeModePassword
+            ForestMode                    = 'WinThreshold'
+        }
+    }
+}
