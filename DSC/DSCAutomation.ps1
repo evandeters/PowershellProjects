7ca2db63d035ccf391eb57e4ffccbf731468a58a
evanjd711 <evanjd711@gmail.com>
2022-08-01 12:03:30 -0700

    we

--- DSC/DSCAutomation.ps1
+++ DSC/DSCAutomation.ps1
@@ -17,7 +17,7 @@ Compress-Archive -Path $env:ProgramFiles\WindowsPowerShell\Modules\cChoco -Desti
 New-DscChecksum -Path $env:ProgramFiles\WindowsPowerShell\DscService\Modules\cChoco_2.5.0.0.zip 
 cd ..\Configuration\OSQuery\
 Publish-MofToPullServer -FullName .\OSQuery.mof -PullServerWebConfig C:\inetpub\PSDSCPullServer\web.config
-cd ..\Configuration\Chrome\
+cd ..\Chrome\
 Publish-MofToPullServer -FullName .\Chrome.mof -PullServerWebConfig C:\inetpub\PSDSCPullServer\web.config
 
 #Client enrollment
@@ -28,10 +28,10 @@ foreach ($Computer in $Computers) {
 }
 $RegistrationKey = Get-Content 'C:\Program Files\WindowsPowerShell\DscService\RegistrationKeys.txt'
 Invoke-Command $Computers -ScriptBlock {
+    Import-Module PSDesiredStateConfiguration
     $Path = Get-Location
     . "C:\PullClientConfigID.ps1"
     PullClientConfigID -DSCServerFQDN $Using:Hostname -Configurations ('OSQuery', 'Chrome') -RegistrationKey $Using:RegistrationKey
-    Start-DscConfiguration -Path "$Path\PullClientConfigID\localhost.meta.mof"
+    pwsh.exe -c 'Set-DscLocalConfigurationManager -Path "$Path\PullClientConfigID\" -Force -Verbose'
     reg add "HKLM\Software\Microsoft\Windows\CurrentVersion\Policies\System" /v DSCAutomationHostEnabled /t REG_DWORD /d 1 /f
-    Restart-Computer -Force
 }
