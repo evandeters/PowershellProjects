7c4c7cdf3a3b234b3d145325bee6864a5b4ab54c
Evan Deters <evanjd711@gmail.com>
2022-08-01 01:18:08 -0700

    client enroll

--- DSC/DSCAutomation.ps1
+++ DSC/DSCAutomation.ps1
@@ -4,7 +4,8 @@ wget https://github.com/evanjd711/PowershellProjects/archive/refs/heads/main.zip
 Expand-Archive $env:ProgramFiles\DSC.zip -DestinationPath $env:ProgramFiles\DSC\
 
 #Install DSC web server
-cd $env:ProgramFiles\DSC\PowershellProjects-main\DSC\InitialConfigs\
+$DSCPath = $env:ProgramFiles\DSC\PowershellProjects-main\DSC\InitialConfigs\
+cd $DSCPath
 Install-Module xPSDesiredStateConfiguration -Confirm:$false
 . .\DscWebServiceRegistration.ps1
 New_xDscPullServer -RegistrationKey (New-Guid).Guid
@@ -18,17 +19,16 @@ cd ..\Configuration\OSQuery\
 Publish-MofToPullServer -FullName .\OSQuery.mof -PullServerWebConfig C:\inetpub\PSDSCPullServer\web.config
 
 #Client enrollment
-New-SMBShare -Name "DSCClients" -Path "$env:ProgramFiles\DSC\PowershellProjects-main\DSC\InitialConfigs\" -FullAccess "Everyone"
+New-SMBShare -Name "DSCClients" -Path $DSCPath -FullAccess "Everyone"
 
 $Computers = Get-ADComputer -filter * | Select-Object -ExpandProperty Name
+$Computers | %{New-PSSession -ComputerName $_; Copy-Item -Path $DSCPath\PullClientConfigID.ps1 -Destination 'C:\' -toSession (Get-PSSession -ComputerName $_)}
 $Hostname = hostname
+$RegistrationKey = Get-Content 'C:\Program Files\WindowsPowerShell\DscService\RegistrationKeys.txt'
 Invoke-Command $Computers -ScriptBlock {
-    mkdir C:\DSC
-    $RegistrationKey = Get-Content 'C:\Program Files\WindowsPowerShell\DscService\RegistrationKeys.txt'
-    cp \\$Hostname\DSCClients\PullClientConfig\PullClientConfigID.ps1 C:\DSC\ClientConfig.ps1
-    . "C:\DSS\ClientConfig.ps1"
-    PullClientConfigID -DSCServerFQDN $Hostname -Configurations ('OSQuery', 'Chrome') -RegistrationKey $RegistrationKey
-    Start-DscConfiguration -Path "C:\DSC\PullClientConfigID"
+    . "C:\ClientConfig.ps1"
+    PullClientConfigID -DSCServerFQDN $Hostname -Configurations ('OSQuery', 'Chrome') -RegistrationKey $Using:RegistrationKey
+    Start-DscConfiguration -Path "C:\PullClientConfigID"
     reg add "HKLM\Software\Microsoft\Windows\CurrentVersion\Policies\System" /v DSCAutomationHostEnabled /t REG_DWORD /d 1 /f
     Restart-Computer
 }
